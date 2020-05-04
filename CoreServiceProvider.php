0820302d7950f4510138a3ddd7ab0e0623ba4877
Vu Ngoc Anh <johnavu.it@gmail.com>
2020-05-04 16:52:57 +0700

    init

---
+++ CoreServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: JohnAVu
+ * Date: 2020-04-12
+ * Time: 22:06
+ */
+namespace Larky\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class CoreServiceProvider extends ServiceProvider{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
